53de24489a2c63e899bf1b87fac749ea3650e4f6
LPLaser <mlzxgzy@gmail.com>
2024-12-18 22:06:04 +0800

    update: 修改菜单顺序

--- sql/V005__competitorMenu.sql
+++ sql/V005__competitorMenu.sql
@@ -1,6 +1,6 @@
 -- 菜单 SQL
 insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('选手', '0', '1', 'competitor', 'exam/competitor/index', 1, 0, 'C', '0', '0', 'exam:competitor:list', '#', 'admin', sysdate(), '', null, '选手菜单');
+values('选手', '0', '20', 'competitor', 'exam/competitor/index', 1, 0, 'C', '0', '0', 'exam:competitor:list', '#', 'admin', sysdate(), '', null, '选手菜单');
 
 -- 按钮父菜单ID
 SELECT @parentId := LAST_INSERT_ID();
