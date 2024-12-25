e29ed26050a2cdd0567b67becae3b70ec279e282
Kami <mlzxgzy@gmail.com>
2024-12-25 16:13:30 +0800

    update: 修改修改排序ID

--- sql/V010__questionAttachmentMenu.sql
+++ sql/V010__questionAttachmentMenu.sql
@@ -1,6 +1,6 @@
 -- 菜单 SQL
 insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('题目附件', '0', '13', 'questionAttachment', 'exam/questionAttachment/index', 1, 0, 'C', '0', '0', 'exam:questionAttachment:list', '#', 'admin', sysdate(), '', null, '题目附件菜单');
+values('题目附件', '0', '32', 'questionAttachment', 'exam/questionAttachment/index', 1, 0, 'C', '0', '0', 'exam:questionAttachment:list', '#', 'admin', sysdate(), '', null, '题目附件菜单');
 
 -- 按钮父菜单ID
 SELECT @parentId := LAST_INSERT_ID();
