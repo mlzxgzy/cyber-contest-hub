edbee4f3d0ec5f415e2b5144fcdbd55757c1ed9c
郭子逸 <mlzxgzy@gmail.com>
2026-01-19 16:25:19 +0800

    feat(menu): 添加题目文件上传下载菜单权限
    - 新增题目文件上传功能菜单项
    - 新增题目文件下载功能菜单项
    - 配置相关权限标识符和菜单层级关系

---
+++ ruoyi-admin/src/main/resources/db/migration/V12__ChallengeFileMenu2.sql
@@ -0,0 +1,10 @@
+INSERT INTO sys_menu (menu_id, menu_name, parent_id, order_num, path, component, query_param, is_frame, is_cache,
+                          menu_type, visible, status, perms, icon, create_dept, create_by, create_time, update_by,
+                          update_time, remark)
+VALUES (2013165189163745281, '题目文件上传', 1997603046311747585, 6, '#', '', null, 1, 0, 'F', '0', '0',
+        'cch:challengeFile:upload', '#', 103, 1, '2026-01-19 16:22:38', null, null, '');
+INSERT INTO sys_menu (menu_id, menu_name, parent_id, order_num, path, component, query_param, is_frame, is_cache,
+                          menu_type, visible, status, perms, icon, create_dept, create_by, create_time, update_by,
+                          update_time, remark)
+VALUES (2013165252439015426, '题目文件下载', 1997603046311747585, 7, '#', '', null, 1, 0, 'F', '0', '0',
+        'cch:challengeFile:download', '#', 103, 1, '2026-01-19 16:22:53', null, null, '');
